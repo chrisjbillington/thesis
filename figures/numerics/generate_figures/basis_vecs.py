45ee50618f0e4f7ceb608bfb03acce00a5f56ff1
Chris Billington <chrisjbillington@gmail.com>
2017-03-30 23:22:03 -0400

    Changes to basis vectors figure

--- figures/numerics/generate_figures/basis_vecs.py
+++ figures/numerics/generate_figures/basis_vecs.py
@@ -51,7 +51,7 @@ import numpy as np
 
 pi = np.pi
 
-n = 7
+n = 13
 
 nx = 1024
 
@@ -63,18 +63,19 @@ f_psi = np.fft.fft2(psi)
 kx = 2*pi*np.fft.fftfreq(n)
 ky = 2*pi*np.fft.fftfreq(n)
 
-x_interp = np.linspace(0, 7, nx, endpoint=False).reshape((1, nx))
-y_interp = np.linspace(0, 7, nx, endpoint=False).reshape((nx, 1))
+x_interp = np.linspace(0, n, nx, endpoint=False).reshape((1, nx))
+y_interp = np.linspace(0, n, nx, endpoint=False).reshape((nx, 1))
 psi_interp = np.zeros((nx, nx), dtype=complex)
 for i in range(n):
     for j in range(n):
-        psi_interp += np.exp(1j*ky[i]*y_interp)*np.exp(1j*kx[j]*x_interp)*f_psi[i, j]
+        psi_interp += (np.exp(1j*ky[i]*y_interp) *
+                       np.exp(1j*kx[j]*x_interp) * f_psi[i, j])
 
 FIG_WIDTH = 4.5
 FIG_HEIGHT = 3.25
 
 fig = plt.figure(figsize=(FIG_WIDTH, FIG_HEIGHT))
-gs = gridspec.GridSpec(4, 1, left=0.075, bottom=0.15,
+gs = gridspec.GridSpec(4, 1, left=0.075, bottom=0.11,
                       right=0.90, top=0.975, wspace=0.5, hspace=0.1)
 
 
@@ -85,7 +86,8 @@ psimax = psi_interp.real.max()
 
 ax1 = plt.subplot(gs[:3,:])
 
-plt.imshow(psi_interp.real, extent=[0, 7, 0, 7], origin='lower', cmap=plt.get_cmap('seismic'),
+plt.imshow(psi_interp.real, extent=[0, n, 0, n], origin='lower',
+           cmap=plt.get_cmap('seismic'),
            vmin=-1.0, vmax=1.0)
 ax1.tick_params(direction='in')
 ax1.xaxis.set_major_formatter(plt.NullFormatter())
@@ -107,20 +109,18 @@ colorbar.set_ticks([-1, -0.5, 0, 0.5, 1])
 plt.savefig('../basis_vecs.pdf', dpi=300)
 
 ax2 = plt.subplot(gs[3,:])
-plt.plot(x_interp.flatten(), psi_interp[int(round(1024*2/7))].real, color=colors['red'])
-plt.grid(True)
-plt.axis([0, 7, -0.3, 1.1])
+plt.plot(x_interp.flatten(), psi_interp[int(round(1024*2/n))].real, color=colors['red'])
+plt.grid(True, linestyle=':')
+plt.axis([0, n, -0.3, 1.3])
 plt.xlabel('$x$')
 plt.ylabel('$\phi_{32}(x, 2)$')
 
 ax1_bbox = ax1.get_position()
 ax2_bbox = ax2.get_position()
-print(ax1_bbox.x0)
-ax2.set_position([0.291589, ax2_bbox.y0, ax1_bbox.width, ax2_bbox.height])
-
-
-ax1_bbox = ax1.get_position()
-ax2_bbox = ax2.get_position()
-print(ax1_bbox.x0)
+ax2.set_position([ax1_bbox.x0, ax2_bbox.y0, ax1_bbox.width, ax2_bbox.height])
+matplotlib.pyplot.xticks(range(1, n+1))
+pts = np.zeros(n+1)
+pts[3] = 1
+plt.plot(range(n+1), pts, 'ko', markersize=1)
 
 plt.savefig('../basis_vecs.pdf', dpi=300)
