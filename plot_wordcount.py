1da145bc36f43294f3d6fff667af454f473b7531
Chris Billington <chrisjbillington@gmail.com>
2017-08-17 23:20:18 -0400

    Writing about FEDVR

--- plot_wordcount.py
+++ plot_wordcount.py
@@ -3,7 +3,7 @@ from subprocess import run, PIPE
 import datetime
 from matplotlib.dates import date2num
 
-os.chdir('/home/cjb7/thesis')
+os.chdir('/home/bilbo/thesis')
 
 wordcounts = []
 dates = []
