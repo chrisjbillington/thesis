4d7391678f4660bade555bf38e87d8dbdbd6afac
chrisjbillington <chrisjbillington@localhost>
2018-10-22 16:55:59 -0400

    Edits in response to examiners' comments, plus a few other errors corrected that were noticed by others.

--- code_listings/expm_example.py
+++ code_listings/expm_example.py
@@ -1,23 +1,23 @@
 import numpy as np
 from numpy.linalg import eigh
 
-def expmh(M):
-    """compute exp(M), where M, shape (..., N, N) is an array of N by N
-    Hermitian matrices, using the diagonalisation method. Made this function
-    because scipy's expm can't take an array of matrices as input, it can only
-    do one at a time."""
+def expiH(H):
+    """compute exp(i*H), where H, shape (..., N, N) is an array of N by N Hermitian
+    matrices, using the diagonalisation method, and where i is the imaginary unit. This
+    fucntion is useful because scipy's expm can't take an array of matrices as input, it
+    can only do one at a time."""
 
     # Diagonalise the matrices:
-    evals, evecs = eigh(M)
+    evals, evecs = eigh(H)
 
-    # Now we compute exp(M) = Q exp(D) Q^\dagger where Q is the matrix of
+    # Now we compute exp(i*H) = Q exp(i*D) Q^\dagger where Q is the matrix of
     # eigenvectors (as columns) and D is the diagonal matrix of eigenvalues:
 
     Q = evecs
     Q_dagger = Q.conj().swapaxes(-1, -2) # Only transpose the matrix dimensions
-    exp_D_diags = np.exp(evals)
+    exp_iD_diags = np.exp(1j*evals)
 
-    # Compute the 3-term matrix product Q*exp_D_diags*Q_dagger using the
+    # Compute the 3-term matrix product Q*exp_iD_diags*Q_dagger using the
     # einsum function in order to specify which array axes of each array to
     # sum over:
-    return np.einsum('...ik,...k,...kj->...ij', Q, exp_D_diags, Q_dagger)
+    return np.einsum('...ik,...k,...kj->...ij', Q, exp_iD_diags, Q_dagger)
